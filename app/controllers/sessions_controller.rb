06cfd696315a992f97d3918340aaa5d1cd8df03c
wrack <carlosveiga1618@gmail.com>
2021-03-22 13:01:27 -0300

    fix issues not returning created user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
-class SessionController < ApplicationController
+class SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params['user']['emails']).try(:authenticate, params['user']['password'])
+    user = User
+      .find_by(email: params['user']['email'])
+      .try(:authenticate, params['user']['password'])
 
     if user
       session[:user_id] = user.id
