b8285c39e5d65d9cb29c7a5bc1d2e31bdc7dda11
wrack <carlosveiga1618@gmail.com>
2021-03-22 14:35:10 -0300

    fix error when creating id in session

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,7 @@ class RegistrationsController < ApplicationController
     )
 
     if user
-      session[:user.id] = user.id
+      session[:user_id] = user.id
       render json: {
         status: :created,
         user: user
