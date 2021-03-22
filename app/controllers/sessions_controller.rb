d47aa5cf7eae15ebeafebe0191aeb0160a86ec30
wrack <carlosveiga1618@gmail.com>
2021-03-22 12:39:22 -0300

    fix rubocop offenses

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionController < ApplicationController
   def create
-    user = User.find_by(email: params["user"]["emails"]).try(:authenticate, params["user"]["password"])
+    user = User.find_by(email: params['user']['emails']).try(:authenticate, params['user']['password'])
   end
+  
 end
