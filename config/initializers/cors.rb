7498d444bd79c89120472bf67803fb4c2f0ad830
wrack <carlosveiga1618@gmail.com>
2021-03-22 12:25:21 -0300

    fix rubocop offenses and add cors config

---
+++ config/initializers/cors.rb
@@ -0,0 +1,8 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origin "*"
+    resourse "*", header: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+
+
+end
