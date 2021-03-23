076f5032082e8b9682a62907b5c0c306aad5c09b
wrack <carlosveiga1618@gmail.com>
2021-03-23 13:55:56 -0300

    fix type of id in favourites table

--- db/migrate/20210323165048_create_favourites.rb
+++ db/migrate/20210323165048_create_favourites.rb
@@ -1,8 +1,8 @@
 class CreateFavourites < ActiveRecord::Migration[6.0]
   def change
     create_table :favourites do |t|
-      t.int :user_id, null: false, foreign_key: true
-      t.int :wod_id, null: false, foreign_key: true
+      t.references :user_id, null: false, foreign_key: true
+      t.references :wod_id, null: false, foreign_key: true
 
       t.timestamps
     end
