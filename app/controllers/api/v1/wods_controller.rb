9a74ac97c681406d1dcf07235c61675661909b03
wrack <carlosveiga1618@gmail.com>
2021-03-23 18:27:06 -0300

    add api and v1 folder and move controllers inside

---
+++ app/controllers/api/v1/wods_controller.rb
@@ -0,0 +1,18 @@
+module Api
+  module V1  
+    class WodsController < ApplicationController
+      include CurrentUserConcern
+
+      def index
+        @wods = Wod.all
+      end
+
+      def show
+        @wod = Wod.find(params[:id])
+        render json: {}
+      end
+
+
+    end
+  end
+end
