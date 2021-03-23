cebbd1243244e4135a1913c63ab138806283d59c
wrack <carlosveiga1618@gmail.com>
2021-03-23 14:48:41 -0300

    add User tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # Association test
+  it { should have_many(:favourites) }
+  it { should have_many(:favourited_wods) }
+
+  # Validation tests
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+
+end
