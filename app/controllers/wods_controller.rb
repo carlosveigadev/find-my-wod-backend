a3a298e1b3a47e4af0a12f951d9e9aead5b9eb45
wrack <carlosveiga1618@gmail.com>
2021-03-23 17:35:48 -0300

    update routing of controllers to proper standards

--- app/controllers/wods_controller.rb
+++ app/controllers/wods_controller.rb
@@ -1,2 +1,3 @@
 class WodsController < ApplicationController
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:create]
-  resources :registrations, only: [:create]
-  delete :logout, to: "sessions#logout"
-  get :logged_in, to: "sessions#logged_in"
-  root to: "static#home"
+  namespace :api do
+    namespace :v1 do
+      resources :sessions, only: [:create]  
+      
+      resources :registrations, only: [:create]
+      
+      delete :logout, to: "sessions#logout"
+      
+      get :logged_in, to: "sessions#logged_in"
+      root to: "wods#index"
+    end
+  end
 end
