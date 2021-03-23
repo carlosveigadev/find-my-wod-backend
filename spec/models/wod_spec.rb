6f2789ba3c7ad111d317e9d82739de8489dd9726
wrack <carlosveiga1618@gmail.com>
2021-03-23 14:47:12 -0300

    add tests to wod model

---
+++ spec/models/wod_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Wod, type: :model do
+  # Association test
+  it { should have_many(:favourites) }
+  it { should have_many(:athletes) }
+
+  # Validation tests
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:image) }
+  it {
+    should validate_length_of(:title)
+      .is_at_least(2)   }
+end
