1a5d5dd8585fb8b3b2a23c070f6842c4d6217816
wrack <carlosveiga1618@gmail.com>
2021-03-23 13:55:10 -0300

    add references to favourites table

--- db/migrate/20210323165048_create_favourites.rb
+++ db/migrate/20210323165048_create_favourites.rb
@@ -1,8 +1,8 @@
 class CreateFavourites < ActiveRecord::Migration[6.0]
   def change
     create_table :favourites do |t|
-      t.int :user_id
-      t.int :wod_id
+      t.int :user_id, null: false, foreign_key: true
+      t.int :wod_id, null: false, foreign_key: true
 
       t.timestamps
     end
