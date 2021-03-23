bc97550b1c97bc642bcacc576e11b5a559080c62
wrack <carlosveiga1618@gmail.com>
2021-03-23 13:57:00 -0300

    fix migrate files of wods

--- db/migrate/20210323165209_create_wods.rb
+++ db/migrate/20210323165209_create_wods.rb
@@ -1,10 +1,10 @@
 class CreateWods < ActiveRecord::Migration[6.0]
   def change
     create_table :wods do |t|
-      t.int :user_id
-      t.varchar :title
-      t.text :description
-      t.text :image
+      t.references :user_id, null: false, foreign_key: true
+      t.varchar :title, null: false
+      t.text :description, null: false
+      t.text :image, null: false
 
       t.timestamps
     end
