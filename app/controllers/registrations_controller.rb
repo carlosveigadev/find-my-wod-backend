52f271eac7bf898d27cfa1ddfb9690b75bc751e3
wrack <carlosveiga1618@gmail.com>
2021-03-23 17:37:40 -0300

    add proper routing to registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,23 @@
-class RegistrationsController < ApplicationController
-  def create
-    user = User.create!(
-      email: params['user']['email'],
-      password: params['user']['password'],
-      password_confirmation: params['user']['password_confirmation']
-    )
+module Api
+  module V1
+    class RegistrationsController < ApplicationController
+      def create
+        user = User.create!(
+          email: params['user']['email'],
+          password: params['user']['password'],
+          password_confirmation: params['user']['password_confirmation']
+        )
 
-    if user
-      session[:user_id] = user.id
-      render json: {
-        status: :created,
-        user: user
-      }
-    else
-      render json: { status: 500 }
+        if user
+          session[:user_id] = user.id
+          render json: {
+            status: :created,
+            user: user
+          }
+        else
+          render json: { status: 500 }
+        end
+      end
     end
   end
 end
--- app/controllers/wods_controller.rb
+++ app/controllers/wods_controller.rb
@@ -1,3 +1,7 @@
-class WodsController < ApplicationController
+module Api
+  module V1  
+    class WodsController < ApplicationController
 
+    end
+  end
 end
