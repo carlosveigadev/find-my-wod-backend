0fd73e0e45339436f3dfd235a3524506c1ced91e
wrack <carlosveiga1618@gmail.com>
2021-03-24 13:23:49 -0300

    refactor User model, add user factory

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates_presence_of :email
-  validates_uniqueness_of :email
-  validates_presence_of :password_digest
+  validates_presence_of :email, :password_digest
+  validates_uniqueness_of :email 
 
   has_many :favourites
   has_many :favourited_wods, through: :favourites, source: :wod
