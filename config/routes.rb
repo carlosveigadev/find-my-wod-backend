55d14272f6f61c50ec57899eeddfa8dadeafb80c
wrack <carlosveiga1618@gmail.com>
2021-03-24 14:20:46 -0300

    remove not necessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do
-      resources :sessions, only: [:create]  
-      
-      resources :registrations, only: [:create]
-      
+    namespace :v1 do      
       resources :wods, only: %i[index show] do
         post 'favourite', to: 'wods#favourite'
         post 'unfavourite', to: 'wods#unfavourite'
       end
 
-      delete :logout, to: "sessions#logout"
-      
-      get :logged_in, to: "sessions#logged_in"
-      
       root to: "wods#index"
     end
   end
