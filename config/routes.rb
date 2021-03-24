de9b971ca2910f042f8305d488c805ee9bd8a068
wrack <carlosveiga1618@gmail.com>
2021-03-24 14:24:25 -0300

    add back sessions_controller and its routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do      
+    namespace :v1 do
+      resources :sessions, only: [:create]  
+      
+      delete :logout, to: "sessions#logout"
+      
+      get :logged_in, to: "sessions#logged_in"
+      
       resources :wods, only: %i[index show] do
         post 'favourite', to: 'wods#favourite'
         post 'unfavourite', to: 'wods#unfavourite'
       end
 
+      
       root to: "wods#index"
     end
   end
