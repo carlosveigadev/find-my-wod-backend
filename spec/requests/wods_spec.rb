b907a7200f32930b748ca9660b8cc7b6e37e2230
wrack <carlosveiga1618@gmail.com>
2021-03-24 14:08:23 -0300

    spec/requests/wods_spec: fix wods_spec requiring user for tests

--- spec/requests/wods_spec.rb
+++ spec/requests/wods_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Wods', type: :request do
   # initialize test data
+  let(:user) { create(:user) }
   let!(:wods) { create_list(:wod, 10) }
   let(:wods_id) { wods.first.id }
+  let(:headers) { valid_headers }
 
   describe 'GET /wods' do
-    before { get '/api/v1/wods' }
+    before { get '/api/v1/wods', params: {}, headers: headers }
+
     it 'returns wods' do
       expect(json).not_to be_empty
       expect(json.size).to eq(10)
@@ -17,7 +20,8 @@ RSpec.describe 'Wods', type: :request do
   end
 
   describe 'GET /wods/:id' do
-    before { get "/api/v1/wods/#{wods_id}" }
+    before { get "/api/v1/wods/#{wods_id}", params: {}, headers: headers }
+
     context 'when wod exists' do
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
