35258a9e36dd84abb882c96928ecb3635ed17112
wrack <carlosveiga1618@gmail.com>
2021-03-30 13:41:26 -0300

    fix rubocop offenses

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,9 +1,9 @@
 class JsonWebToken
-  if Rails.env.production?
-    HMAC_SECRET = ENV['SECRET_KEY_BASE']
-  else
-    HMAC_SECRET = Rails.application.secrets.secret_key_base
-  end
+  HMAC_SECRET = if Rails.env.production?
+                  ENV['SECRET_KEY_BASE']
+                else
+                  Rails.application.secrets.secret_key_base
+                end
 
   def self.encode(payload, exp = 24.hours.from_now)
     # set expiry to 24 hours from creation time
