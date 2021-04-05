3d96a1b3a07bbe01cb013d20eaaefb598a8d0802
wrack <carlosveiga1618@gmail.com>
2021-04-05 14:38:16 -0300

    fix rubocop ofenses

--- app/controllers/api/v1/favourites_controller.rb
+++ app/controllers/api/v1/favourites_controller.rb
@@ -1,14 +1,13 @@
 module Api
   module V1
     class FavouritesController < ApplicationController
-
       def index
         favs = current_user.favourited_wods
         json_response(favs)
       end
 
       def create
-        favourites = Favourite.create(user_id: current_user.id, wod_id: params[:favourite][:wod_id])
+        Favourite.create(user_id: current_user.id, wod_id: params[:favourite][:wod_id])
         render json: { message: 'Success' }
       end
 
@@ -23,7 +22,6 @@ module Api
       def fav_params
         params.require(:wod_id).permit(:favourite)
       end
-
     end
   end
 end
--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -2,5 +2,5 @@ class Favourite < ApplicationRecord
   belongs_to :wod
   belongs_to :user
 
-  validates_uniqueness_of :wod_id, :scope => [:user_id]
+  validates_uniqueness_of :wod_id, scope: [:user_id]
 end
