e9be244fc9fe0dc75997a3e2a96b5f4cf1bbf5bc
wrack <carlosveiga1618@gmail.com>
2021-04-13 12:23:48 -0300

    fix rubocop offenses

--- app/controllers/api/v1/favourites_controller.rb
+++ app/controllers/api/v1/favourites_controller.rb
@@ -2,19 +2,19 @@ module Api
   module V1
     class FavouritesController < ApplicationController
       def index
-        current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+        current_user = AuthorizeApiRequest.new(request.headers).call[:user]
         favs = current_user.favourited_wods
         json_response(favs)
       end
 
       def create
-        current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+        current_user = AuthorizeApiRequest.new(request.headers).call[:user]
         Favourite.create(user_id: current_user.id, wod_id: params[:favourite][:wod_id])
         render json: { message: 'Success' }
       end
 
       def destroy
-        current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+        current_user = AuthorizeApiRequest.new(request.headers).call[:user]
         favourite = current_user.favourites.find_by(wod_id: params[:id])
         favourite&.delete
         render json: { message: 'Successfull Deleted' }
